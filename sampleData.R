a3ca96fec4336bf6d4f662cad3fc7b317d662fae
riwinters <palashcool10@gmail.com>
2018-11-26 00:47:25 +0530

    Add files via upload

---
+++ sampleData.R
@@ -0,0 +1,9 @@
+sampleData <- function(tmpData, percent = 0.05) {
+  
+  numLines <- length(tmpData)
+  
+  tmpData <- sample(tmpData, size = numLines * percent, replace = FALSE)
+  
+  return(tmpData)
+}
+
